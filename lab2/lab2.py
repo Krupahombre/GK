6b8290dca594c905dd4f171a9d87dd3e12b0b465
Krupahombre <kacper.krupinski01@gmail.com>
2022-11-24 11:57:24 +0100

    Added Lab2-4

---
+++ lab2/lab2.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+import sys
+import random
+
+from glfw.GLFW import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+d = random.random()
+
+RED_VALUE = random.random()
+GREEN_VALUE = random.random()
+BLUE_VALUE = random.random()
+
+def startup():
+    update_viewport(None, 400, 400)
+    glClearColor(0.5, 0.5, 0.5, 1.0)
+
+
+def shutdown():
+    pass
+
+def render(time):
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    # zad 1
+    #drawTriangle()
+
+    # zad 2
+    #drawRectangle(0, 0, 160, 60)
+
+    # zad 3
+    #drawRectangleDeform(0, 0, 160, 60, d)
+
+    # zad 4
+    drawSierpinskiCarpet(0, 0, 150, 120, 3)
+
+    glFlush()
+
+def drawTriangle():
+    # zmiana koloru wierzchołka przed jego utworzeniem
+
+    glColor3f(0.0, 0.0, 0.0)
+    glBegin(GL_TRIANGLES)
+    glColor3ub(255, 0, 0)
+    glVertex2f(50.0, 0.0)
+    glColor3ub(0, 255, 0)
+    glVertex2f(0.0, 50.0)
+    glColor3ub(0, 0, 255)
+    glVertex2f(-50.0, 0.0)
+    glEnd()    
+
+def drawRectangle(x, y, width, height):
+    glColor3f(0.6, 0.3, 0.1)
+    glBegin(GL_TRIANGLES)
+    glVertex2f(x-(width/2), y+(height/2))
+    glVertex2f(x-(width/2), y-(height/2))
+    glVertex2f(x+(width/2), y-(height/2))
+    glEnd()
+
+    glColor3f(0.6, 0.3, 0.1)
+    glBegin(GL_TRIANGLES)
+    glVertex2f(x-(width/2), y+(height/2))
+    glVertex2f(x+(width/2), y+(height/2))
+    glVertex2f(x+(width/2), y-(height/2))
+    glEnd()   
+
+def drawRectangleDeform(x, y, width, height, d = 0.0):
+    glColor3f(RED_VALUE, GREEN_VALUE, BLUE_VALUE)
+    glBegin(GL_TRIANGLES)
+    glVertex2f((x-(width/2)) * d, (y+(height/2)) * d)
+    glVertex2f((x-(width/2)) * d, (y-(height/2)) * d)
+    glVertex2f((x+(width/2)) * d, (y-(height/2)) * d)
+    glEnd()
+
+    glColor3f(RED_VALUE, GREEN_VALUE, BLUE_VALUE)
+    glBegin(GL_TRIANGLES)
+    glVertex2f((x-(width/2)) * d, (y+(height/2)) * d)
+    glVertex2f((x+(width/2)) * d, (y+(height/2)) * d)
+    glVertex2f((x+(width/2)) * d, (y-(height/2)) * d)
+    glEnd()
+
+# rysowanie dywanu na zasadzie rysowania prostokątów dookoła pustego miejsca na środku; wywoływanie rekurencyjne
+def drawSierpinskiCarpet(x, y, width, height, level):
+    if level > 0:
+        level -= 1
+        width /= 3
+        height /= 3
+
+        drawSierpinskiCarpet(x - width, y + height, width, height, level) # lewo-góra
+        drawSierpinskiCarpet(x, y + height, width, height, level) # góra
+        drawSierpinskiCarpet(x + width, y + height, width, height, level) # prawo-góra
+        drawSierpinskiCarpet(x + width, y, width, height, level) # prawo
+        drawSierpinskiCarpet(x + width, y - height ,width, height, level) # prawo-dół
+        drawSierpinskiCarpet(x, y - height, width, height, level) # dół
+        drawSierpinskiCarpet(x - width, y - height, width, height, level) # lewo-dół
+        drawSierpinskiCarpet(x - width, y, width, height, level) # lewo
+
+    else:
+        drawRectangle(x, y, width, height)
+
+
+def update_viewport(window, width, height):
+    if width == 0:
+        width = 1
+    if height == 0:
+        height = 1
+    aspect_ratio = width / height
+
+    glMatrixMode(GL_PROJECTION)
+    glViewport(0, 0, width, height)
+    glLoadIdentity()
+
+    if width <= height:
+        glOrtho(-100.0, 100.0, -100.0 / aspect_ratio, 100.0 / aspect_ratio,
+                1.0, -1.0)
+    else:
+        glOrtho(-100.0 * aspect_ratio, 100.0 * aspect_ratio, -100.0, 100.0,
+                1.0, -1.0)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+
+def main():
+    if not glfwInit():
+        sys.exit(-1)
+
+    window = glfwCreateWindow(400, 400, __file__, None, None)
+    if not window:
+        glfwTerminate()
+        sys.exit(-1)
+
+    glfwMakeContextCurrent(window)
+    glfwSetFramebufferSizeCallback(window, update_viewport)
+    glfwSwapInterval(1)
+
+    startup()
+    while not glfwWindowShouldClose(window):
+        render(glfwGetTime())
+        glfwSwapBuffers(window)
+        glfwPollEvents()
+    shutdown()
+
+    glfwTerminate()
+
+
+if __name__ == '__main__':
+    main()
---
+++ lab3/lab3.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python3
+import sys
+
+import numpy as nump
+import random as rand
+
+from glfw.GLFW import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+
+def startup():
+    update_viewport(None, 400, 400)
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+    glEnable(GL_DEPTH_TEST)
+
+    global N
+    global verticesArray
+    global colorArray
+
+    N = 25
+    verticesArray = createVerticesArray(N)
+    colorArray = createRandomColorArray(N)
+
+
+def createVerticesArray(N):
+    verticesArray = nump.zeros((N, N, 3))
+
+# utworzenie tabeli N elementowej z wypełnieniem komórek wartościami od 0 do 1 w zależności od N
+    uArray: list = [u / (N - 1) for u in range(0, N)]
+    vArray: list = [v / (N - 1) for v in range(0, N)]
+
+    for i in range(0, N):
+        for j in range(0, N):
+            # x
+            verticesArray[i][j][0] = ((-90 * uArray[i] ** 5
+                                   + 225 * uArray[i] ** 4
+                                   - 270 * uArray[i] ** 3
+                                   + 180 * uArray[i] ** 2
+                                   - 45 * uArray[i])
+                                   * nump.cos(nump.pi * vArray[j]))
+            # y
+            verticesArray[i][j][1] = (160 * uArray[i] ** 4
+                                   - 320 * uArray[i] ** 3
+                                   + 160 * uArray[i] ** 2 - 5)
+            # z
+            verticesArray[i][j][2] = ((-90 * uArray[i] ** 5
+                                    + 225 * uArray[i] ** 4
+                                    - 270 * uArray[i] ** 3
+                                    + 180 * uArray[i] ** 2
+                                    - 45 * uArray[i])
+                                   * nump.sin(nump.pi * vArray[j]))
+
+    return verticesArray
+
+
+def createRandomColorArray(N):
+    randomColorArray = nump.zeros((N, N, 3))
+
+    for i in range(0, N):
+        for j in range(0, N):
+            randomColorArray[i][j][0] = rand.random()
+            randomColorArray[i][j][1] = rand.random()
+            randomColorArray[i][j][2] = rand.random()
+
+    return randomColorArray
+
+
+def shutdown():
+    pass
+
+
+def drawEggModelWithPoints():
+    glBegin(GL_POINTS)
+    glColor3f(1.0, 0.0, 1.0)
+    for i in range(0, N):
+        for j in range(0, N):
+            glVertex3f(verticesArray[i][j][0],
+                          verticesArray[i][j][1],
+                          verticesArray[i][j][2])
+    glEnd()
+
+
+def drawEggModelWithLines():
+    glColor3f(1.0, 0.0, 1.0)
+    for i in range(0, N - 1):
+        for j in range(0, N - 1):
+            glBegin(GL_LINES)
+            glVertex3f(verticesArray[i][j][0],
+                          verticesArray[i][j][1],
+                          verticesArray[i][j][2])
+            glVertex3f(verticesArray[i + 1][j][0],
+                          verticesArray[i + 1][j][1],
+                          verticesArray[i + 1][j][2])
+            glEnd()
+
+            glBegin(GL_LINES)
+            glVertex3f(verticesArray[i][j][0],
+                          verticesArray[i][j][1],
+                          verticesArray[i][j][2])
+            glVertex3f(verticesArray[i][j + 1][0],
+                          verticesArray[i][j + 1][1],
+                          verticesArray[i][j + 1][2])
+            glEnd()    
+
+
+def drawEggModelWithTriangles():
+    for i in range(0, N - 1):
+        for j in range(0, N - 1):
+            glBegin(GL_TRIANGLES)
+            glColor3f(colorArray[i][j][0],
+                        colorArray[i][j][1],
+                        colorArray[i][j][2])
+            glVertex3f(verticesArray[i][j][0],
+                        verticesArray[i][j][1],
+                        verticesArray[i][j][2])
+
+            glColor3f(colorArray[i + 1][j][0],
+                        colorArray[i + 1][j][1],
+                        colorArray[i + 1][j][2])
+            glVertex3f(verticesArray[i + 1][j][0],
+                        verticesArray[i + 1][j][1],
+                        verticesArray[i + 1][j][2])
+
+            glColor3f(colorArray[i][j + 1][0],
+                        colorArray[i][j + 1][1],
+                        colorArray[i][j + 1][2])
+            glVertex3f(verticesArray[i][j + 1][0],
+                        verticesArray[i][j + 1][1],
+                        verticesArray[i][j + 1][2])
+            glEnd()
+
+            glBegin(GL_TRIANGLES)
+            glColor3f(colorArray[i + 1][j][0],
+                        colorArray[i + 1][j][1],
+                        colorArray[i + 1][j][2])
+            glVertex3f(verticesArray[i + 1][j][0],
+                        verticesArray[i + 1][j][1],
+                        verticesArray[i + 1][j][2])
+
+            glColor3f(colorArray[i][j + 1][0],
+                        colorArray[i][j + 1][1],
+                        colorArray[i][j + 1][2])
+            glVertex3f(verticesArray[i][j + 1][0],
+                        verticesArray[i][j + 1][1],
+                        verticesArray[i][j + 1][2])
+
+            glColor3f(colorArray[i + 1][j + 1][0],
+                        colorArray[i + 1][j + 1][1],
+                        colorArray[i + 1][j + 1][2])
+            glVertex3f(verticesArray[i + 1][j + 1][0],
+                        verticesArray[i + 1][j + 1][1],
+                        verticesArray[i + 1][j + 1][2])
+            glEnd()
+
+def drawEggModelWIthTriangleStrip():
+    glBegin(GL_TRIANGLE_STRIP)
+    for i in range(N - 1):
+        for j in range(N - 1):
+            if (j != N - 2):
+                glColor3f(colorArray[i][j][0],
+                             colorArray[i][j][1],
+                             colorArray[i][j][2])
+                glVertex3f(verticesArray[i][j][0],
+                              verticesArray[i][j][1],
+                              verticesArray[i][j][2])
+                glColor3f(colorArray[i][j + 1][0],
+                             colorArray[i][j + 1][1],
+                             colorArray[i][j + 1][2])
+                glVertex3f(verticesArray[i][j + 1][0],
+                              verticesArray[i][j + 1][1],
+                              verticesArray[i][j + 1][2])
+                glColor3f(colorArray[i + 1][j][0],
+                             colorArray[i + 1][j][1],
+                             colorArray[i + 1][j][2])
+                glVertex3f(verticesArray[i + 1][j][0],
+                              verticesArray[i + 1][j][1],
+                              verticesArray[i + 1][j][2])
+                glColor3f(colorArray[i + 1][j + 1][0],
+                             colorArray[i + 1][j + 1][1],
+                             colorArray[i + 1][j + 1][2])
+                glVertex3f(verticesArray[i + 1][j + 1][0],
+                              verticesArray[i + 1][j + 1][1],
+                              verticesArray[i + 1][j + 1][2])
+            else:
+                glColor3f(colorArray[i][j][0],
+                             colorArray[i][j][1],
+                             colorArray[i][j][2])
+                glVertex3f(verticesArray[i][j][0],
+                              verticesArray[i][j][1],
+                              verticesArray[i][j][2])
+                glColor3f(colorArray[i][j + 1][0],
+                             colorArray[i][j + 1][1],
+                             colorArray[i][j + 1][2])
+                glVertex3f(verticesArray[i][j + 1][0],
+                              verticesArray[i][j + 1][1],
+                              verticesArray[i][j + 1][2])
+                glColor3f(colorArray[i + 1][j][0],
+                             colorArray[i + 1][j][1],
+                             colorArray[i + 1][j][0])
+                glVertex3f(verticesArray[i + 1][j][0],
+                              verticesArray[i + 1][j][1],
+                              verticesArray[i + 1][j][2])
+                glColor3f(colorArray[i + 1][j + 1][0],
+                             colorArray[i + 1][j + 1][1],
+                             colorArray[i + 1][j + 1][0])
+                glVertex3f(verticesArray[i + 1][j + 1][0],
+                              verticesArray[i + 1][j + 1][1],
+                              verticesArray[i + 1][j + 1][2])
+    glEnd()            
+
+
+def spin(angle):
+    glRotatef(angle, 1.0, 0.0, 0.0)
+    glRotatef(angle, 0.0, 1.0, 0.0)
+    glRotatef(angle, 0.0, 0.0, 1.0)
+
+
+def axes():
+    glBegin(GL_LINES)
+
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex3f(-1.0, 0.0, 0.0)
+    glVertex3f(1.0, 0.0, 0.0)
+
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f(0.0, -1.0, 0.0)
+    glVertex3f(0.0, 1.0, 0.0)
+
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex3f(0.0, 0.0, -1.0)
+    glVertex3f(0.0, 0.0, 1.0)
+
+    glEnd()
+
+
+def render(time):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+
+    axes()
+
+    # zad 1
+    #drawEggModelWithPoints()
+
+    # zad 2
+    #spin(time * 180 / nump.pi)
+    #drawEggModelWithLines()
+
+    # zad 3
+    #spin(time * 180 / nump.pi)
+    #drawEggModelWithTriangles()
+
+    # zad 4
+    spin(time * 180 / nump.pi)
+    drawEggModelWIthTriangleStrip()
+
+    glFlush()
+
+
+def update_viewport(window, width, height):
+    if width == 0:
+        width = 1
+    if height == 0:
+        height = 1
+    aspect_ratio = width / height
+
+    glMatrixMode(GL_PROJECTION)
+    glViewport(0, 0, width, height)
+    glLoadIdentity()
+
+    if width <= height:
+        glOrtho(-7.5, 7.5, -7.5 / aspect_ratio, 7.5 / aspect_ratio, 7.5, -7.5)
+    else:
+        glOrtho(-7.5 * aspect_ratio, 7.5 * aspect_ratio, -7.5, 7.5, 7.5, -7.5)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+
+def main():
+    if not glfwInit():
+        sys.exit(-1)
+
+    window = glfwCreateWindow(400, 400, __file__, None, None)
+    if not window:
+        glfwTerminate()
+        sys.exit(-1)
+
+    glfwMakeContextCurrent(window)
+    glfwSetFramebufferSizeCallback(window, update_viewport)
+    glfwSwapInterval(1)
+
+    startup()
+    while not glfwWindowShouldClose(window):
+        render(glfwGetTime())
+        glfwSwapBuffers(window)
+        glfwPollEvents()
+    shutdown()
+
+    glfwTerminate()
+
+
+if __name__ == '__main__':
+    main()
